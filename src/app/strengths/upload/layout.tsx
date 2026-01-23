5abafbbd3449882bca8e64c805fba7c16738c115
Larry <lcortez86@icloud.com>
2026-01-23 12:15:24 -0700

    Delete layout.tsx

--- src/app/strengths/upload/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DashboardLayout } from "@/components/layout/DashboardLayout";
-
-export default function UploadStrengthsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
